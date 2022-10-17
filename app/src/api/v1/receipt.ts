1a474b1d628a47fa96b62d9b6b9f381f30f5d507
y2s1982 <y2s1982@gmail.com>
2022-10-16 21:58:28 -0400

    feat: method to add receipts.

--- app/src/api/v1/receipt.ts
+++ app/src/api/v1/receipt.ts
@@ -1,9 +1,23 @@
 import Router from "koa-router";
+import Koa from "koa";
+import { getUserId, getEntry } from "./helper/common";
+import { saveReceipt } from "./helper/image";
+import { Entry } from "./state";
 
 const receipt: Router = new Router();
+
+async function * addReceipt (ctx: Koa.Context, next: Function) {
+  const id = getUserId(ctx);
+  const inferEntry = saveReceipt(id,ctx.files.file[0].buffer);
+  ctx.body = inferEntry;
+  await next();
+}
+
 receipt.get("/", async (ctx, next) => {
   ctx.body = { msg: "Hello Receipt!" };
   await next();
 });
+
+receipt.post("/add", addReceipt);
 
 export = receipt;
