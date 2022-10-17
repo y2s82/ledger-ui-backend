9f2958b6de1335e6d8c5272219c1400d968fae95
y2s1982 <y2s1982@gmail.com>
2022-10-17 00:25:19 -0400

    feat: add ways to CRUD entry.

--- app/src/api/v1/v1api.ts
+++ app/src/api/v1/v1api.ts
@@ -1,6 +1,7 @@
 import Router from "koa-router";
 import user from "./user";
 import receipt from "./receipt";
+import entry from "./entry";
 
 const v1api: Router = new Router();
 
@@ -10,6 +11,7 @@ v1api.get("/", async (ctx, next) => {
 });
 
 v1api.use('/user', user.routes(), user.allowedMethods());
+v1api.use('/entry', entry.routes(), entry.allowedMethods());
 v1api.use('/receipt', receipt.routes(), receipt.allowedMethods());
 
 export = v1api;
